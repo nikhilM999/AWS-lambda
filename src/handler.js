e4763c37f6793b2ffd5efff4b386608901821573
nikhilM999 <nikhil.m@niro.money>
2023-09-19 22:22:42 +0530

    2nd commit

---
+++ src/handler.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports.add = async (a,b) => {
+  return 
+    a+b;
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
